d15adcdab3c8221b39fce53eeb5f63e78a346a8a
Pranav <dpranav301@gmail.com>
2023-02-02 14:35:17 +0530

    Linaer is now change with Binary

--- linear.java
+++ linear.java
@@ -1,12 +1,26 @@
 public class linear {
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4, 5 };
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == 5) {
-                System.out.println("Element found");
+        int a = 0;
+        int b = arr.length - 1;
+        while (a > b) {
+            int mid = (a + b) / 2;
+            if (arr[mid] == 10) {
+                System.out.println("Element is found");
+            } else if (arr[mid] > 10) {
+
+                b = mid - 1;
             } else {
-                System.out.println("Element not found");
+                a = mid + 1;
             }
         }
+        // for (int i = 0; i < arr.length; i++) {
+        // if (arr[i] == 5) {
+        // System.out.println("Element found");
+        // } else {
+        // System.out.println("Element not found");
+        // }
+        // }
+
     }
 }
