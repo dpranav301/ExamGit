2ca4ca837da25a0acc9e837d85249fd3dab9efd9
Pranav <dpranav301@gmail.com>
2023-02-02 14:26:08 +0530

    First Linear Commit

---
+++ linear.java
@@ -0,0 +1,12 @@
+public class linear {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 5) {
+                System.out.println("Element found");
+            } else {
+                System.out.println("Element not found");
+            }
+        }
+    }
+}
